7e5ba39a007fb6069d3f80ece4ccc8bf2f7c1820
Peter Peck <karatekid4christ@gmail.com>
2018-02-09 15:42:38 -0600

    Fixing jumbotron

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,7 +3,7 @@ import "./Jumbotron.css";
 
 const Jumbotron = props => (
     <div className="jumbotron">
-        <h1>Click on your favorite Looney Tunes character to begin!
+        <h1>Click on your favorite Looney Tunes character to begin.
         Click on an image to earn points, but don't click anymore than once!</h1>
     </div>
 );
