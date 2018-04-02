7cba3c5906cb42df977090702045c565a2539e9a
Peter Peck <karatekid4christ@gmail.com>
2018-04-02 17:54:57 -0500

    Added new image for tweety

--- src/components/Body/characters.js
+++ src/components/Body/characters.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./characters.css";
 
 const CharacterCard = props => (
-        <button onClick={() => props.handleOnchange(props.id, props.phrase)}>
-                <div className="cards">
-                        <div className="img-container">
-                                <img id={props.id} alt={props.name} src={props.image} />
-                        </div>
-                </div>
-        </button>
+  <button onClick={() => props.handleOnchange(props.id, props.phrase)}>
+    <div className="cards">
+      <div className="img-container">
+        <img id={props.id} alt={props.name} src={props.image} />
+      </div>
+    </div>
+  </button>
 );
 
 export default CharacterCard;
